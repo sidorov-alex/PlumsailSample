b761d79bf3b9db778e9a7af4da0ffb0aba88b3e4
sidorov <sa86@pm.me>
2019-10-27 19:39:12 +1000

    Добавлен поиск по имени в списке.

--- src/ClientApp/src/app/contact-list/contact-list.component.ts
+++ src/ClientApp/src/app/contact-list/contact-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from 'src/app/contact.service';
 import { Contact } from '../contact';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact-list',
@@ -15,7 +16,20 @@ export class ContactListComponent implements OnInit {
 
   ngOnInit() {
     this.contactService.getList()
-      .subscribe(list => this.contacts = list);
+      .subscribe(list => {
+        this.contacts = list;
+        this.filteredContacts = list;
+      });
   }
 
+  onSearchChange(searchText: string): void {
+    searchText = searchText.toLowerCase();
+
+    this.filteredContacts = this.contacts.filter(item => {
+      return item.firstName.toLowerCase().indexOf(searchText) != -1 ||
+             item.lastName.toLowerCase().indexOf(searchText) != -1;
+    });
+  }
+
+  filteredContacts: Contact[];
 }
